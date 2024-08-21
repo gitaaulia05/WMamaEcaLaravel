29dfb273c18908e10316665f84369e886625b37a
sofia nuria <sofianuria09@gmail.com>
2024-08-21 20:42:08 +0700

    routes di view

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,7 @@ use App\Http\Controllers\KasbonController;
 Route::get('/', [KasbonController::class, 'index']);
 Route::get('/{id_barang}', [KasbonController::class, 'detail_data']);
 
-
+Route::get('/login', function() {
+    return view('login');
+});
 
