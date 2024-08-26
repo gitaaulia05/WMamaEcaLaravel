4ea87a9ccd73d09ca8dfbb2cfe9ce76b61a969d4
sofia nuria <sofianuria09@gmail.com>
2024-08-27 01:18:17 +0700

    tambah-data

--- resources/views/admin/tambah_data.blade.php
+++ resources/views/admin/tambah_data.blade.php
@@ -3,54 +3,45 @@
 
 @section('container')
 
-<style>
-        .container {
-            height: 400px;
-            margin-left: 30px;
-        }
+<div class="container mt-4" style="min-height: 400px; margin-left: 30px; background-color: #EEEEEE; padding-bottom: 130px; position: relative;">
 
-        .custom-textarea {
-            width: 30%;
-            height: 200px;
-        }
-</style>
-
-<div class="container mt-4">
     <form class= "form" action="/tambah-data" method="POST">
         @csrf
 
         <div class="form-group mb-3">
-        <label for="name" style="font-size: 15px;">Nama Barang:</label>
+        <label for="name" >Nama Barang:</label>
             <input type="text" id="name" name="name" class="form-control" style="width: 30%;" required>
         </div>
 
         <div class="form-group mb-3">
-            <label for="price" style="font-size: 15px;">Harga Barang:</label>
+            <label for="price" >Harga Barang:</label>
             <input type="number" id="price" name="price" class="form-control" style="width: 25%;" required>
         </div>
 
         <div class="form-group mb-3">
-            <label for="stock" style="font-size: 15px;">Stok Barang:</label>
+            <label for="stock" >Stok Barang:</label>
             <input type="number" id="stock" name="stock" class="form-control" style="width: 20%;" required>
         </div>
 
         <div class="form-group mb-3">
-            <label for="description" style="font-size: 15px;">Deskripsi:</label>
-            <textarea id="description" name="description" class="form-control custom-textarea" required></textarea>
+            <label for="description">Deskripsi:</label>
+            <textarea id="description" name="description" class="form-control" style="width: 30%; height: 200px;" required></textarea>
+
         </div>
 
         <div class="form-group mb-3">
-            <label for="date" style="font-size: 15px;">Tanggal Input:</label>
+            <label for="date">Tanggal Input:</label>
             <input type="date" id="date" name="date" class="form-control" style="width: 18%;" required>
         </div>
 
         <div class="form-group mb-3">
-            <input type="file" id="product_img" name="product_img" style="width: 100%; align-items: center;" required>
-            <label for="product_img" style="display: block;  margin-top: 5px;">Gambar Barang</label>
+            <div style="position: absolute; top: 300px; right: 70px; width: 20%;">
+                <input type="file" id="image" name="image" class="form-control" required>
+                <label for="image">Gambar Barang</label>
+            </div>
         </div>
 
-
-        <button type="submit" class="btn btn-primary" style="background-color: #ff6f5e; color: white; float: right;">Tambah Data</button>
+        <button type="submit" class="btn btn-primary" style="background-color: #ff6f5e; color: white; float: right; margin-top: 50px; ">Tambah Data</button>
     </form>
 </div>
 
