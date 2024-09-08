d39f17022e99415c10fe9009821e5343445f6832
gitaaulia05 <gitaauliahafid@gmail.com>
2024-09-08 23:00:41 +0700

    fixing-detail-rinci-back

--- resources/views/admin/kasbon/detail_data.blade.php
+++ resources/views/admin/kasbon/detail_data.blade.php
@@ -3,22 +3,10 @@
 {{-- //  INI HAPUS AJA LINK BOOTSTRAP NYA CUMAN BUAT TEST AJA --}}
 
 
-    {{-- @foreach ($data->detKasbon as $d)
-          <div class="bg-danger mx-auto col-5">
-    <h1>cicilan ke {{$d->cicilan_ke}}</h1>
-     @if ($user)
-         <h1> nama pembeli : {{$user->nama}}</h1>
-     @endif
-              @foreach ($pembelian->detail_pembelian as $detail)
-         <h1>Nama Barang : {{$detail->namaBarang->nama_barang}}</h1>
-     @endforeach
-        <h1>total bayar : {{$d->total_bayar}}</h1>
-        <h1>tanggal Bayar : {{$d->created_at}}</h1>
-</div>
-    @endforeach --}}
+
 
     @extends('admin.template.aside')
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 @section('container')
 
 
@@ -79,7 +67,7 @@
                       </span
                      </td>
                       <td class="align-middle text-center">
-                      <a href="/detail-kasbon-rinci/" class="btn btn-orange"
+                      <a href="/detail-kasbon-rinci/{{$d->slug}}" class="btn btn-orange"
                             style="background-color: #ff8567; color: white; padding: 0.25rem 0.75rem; border-radius: 0.25rem; border: none; margin: 5px; text-transform: none;">Detail
                         </a>
                       </td>
--- resources/views/admin/kasbon/detail_rinci.blade.php
+++ resources/views/admin/kasbon/detail_rinci.blade.php
@@ -1,7 +1,23 @@
 @extends('admin.template.aside')
-
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 @section('container')
 
-<p>hi</p>
+    <div class="bg-danger mx-auto col-5">
+   <h1>nama pembeli : {{$data->users->nama}}</h1> 
+   <h1>total bayar : {{$data->total_bayar}}</h1> 
+
+  @foreach ($data->detail_pembelian as $dp)
+            <h1> nama barang : {{ $dp->namaBarang->nama_barang }}</h1>
+    
+       <h1> jumlah barang : {{ $dp->kuantitas}}</h1>
+    @endforeach
+
+      @foreach ($data->kasbon as $k)
+            <h1>{{ $k->total_bayar }}</h1>
+    
+    @endforeach
+    </div>
+
+
 
 @endsection
