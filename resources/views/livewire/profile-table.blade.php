3fb8a842aff4238d9c3b9ef4b7a73a429525e4a3
sofia nuria <sofianuria09@gmail.com>
2024-09-18 23:55:39 +0700

    edit-dikit-product

--- resources/views/livewire/profile-table.blade.php
+++ resources/views/livewire/profile-table.blade.php
@@ -15,7 +15,7 @@
               <h6 class="text-center">Pesanan Anda</h6>
             </div>
 
-            
+
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0 ms-3">
                 <table class="table align-items-center mb-0">
@@ -26,32 +26,32 @@
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Aksi</th>
                     </tr>
                   </thead>
-              
+
                   <tbody>
               @foreach($data as $d)
                     <tr>
-                        
+
                       <td>
                         <div class="d-flex px-2 py-1">
                             <img src="{{asset('images/img/team-2.jpg')}}" class="avatar avatar-sm me-3" alt="user1">
                         </div>
                       </td>
 
-              
+
                       <td>
                           @foreach($d->detail_pembelian as $dp)
                         <p class="text-xs font-weight-bold mb-0">{{$dp->namaBarang->nama_barang}}</p>
                          @endforeach
                       </td>
-                    
+
 
                       <td class="align-middle text-center">
-                      
+
                       <a href="/pesanan/{{$d->slug}}" class="btn btn-orange"
                             style="background-color: #ff8567; color: white; padding: 0.25rem 0.75rem; border-radius: 0.25rem; border: none; margin: 5px; text-transform: none;">Detail
                         </a>
                       </td>
-                        
+
                     </tr>
                                 @endforeach
 
@@ -62,7 +62,7 @@
             </div>
           </div>
         </div>
-  
+
     </div>
 
 </div>
--- resources/views/user/pesanan.blade.php
+++ resources/views/user/pesanan.blade.php
@@ -3,17 +3,28 @@
     @section('container')
       @foreach ($data as $d)
               @foreach ($d->detail_pembelian as $dp)
+              <div class="flex justify-center items-center min-h-screen">
+              <div class="card shadow-lg w-45 flex flex-row items-center p-4 ms-8">
 
-        <div class="card shadow-lg w-64">
-                <img src="{{asset('images/img/team-2.jpg')}}" class="avatar avatar-sm me-3" alt="user1">
-                <h1>Nama Produk : {{ $dp->namaBarang->nama_barang}}</h1>
-                <h1>Kuantitas : {{$dp->kuantitas}}</h1>
-                <h1>Harga : {{$dp->harga_perProduk}} </h1>
-                <h1>Tanggal Pesanan  :{{$dp->created_at->format(' d-m-Y  h:i')}}</h1>
-        </div>
+              <div class="w-1/3">
+                <img src="{{asset('images/img/team-2.jpg')}}" class="avatar avatar-sm me-3" alt="user1" style="width: 100%; height: auto;">
+              </div>
+
+              <div class="w-2/3 pl-4">
+                <h1 class="text-lg">Nama Produk: {{ $dp->namaBarang->nama_barang}}</h1>
+                <h1 class="text-lg">Kuantitas: {{$dp->kuantitas}}</h1>
+                <h1 class="text-lg">Harga: {{$dp->harga_perProduk}} </h1>
+                <h1 class="text-lg">Tanggal Pesanan:{{$dp->created_at->format(' d-m-Y  h:i')}}</h1>
+              </div>
+          </div>
+      </div>
 
            @endforeach
            @endforeach
 
-           <a href="/profile">Kembali </a>
+           <div class="flex flex-col justify-end items-center min-h-screen p-4 ms-12">
+                <div class="button-dashboard" >
+                    <a href="/profile" class="btn btn-orange" style="background-color: #ff8567; color: #ffffff; border: none;">Kembali </a>
+                </div>
+           </div>
     @endsection
